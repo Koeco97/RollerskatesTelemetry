0f0a76d46ccb408e12fdaa17ecc2ec5ff8093ce8
Koeco97 <koeco97@gmail.com>
2020-10-05 10:17:41 +0300

    rewritten KafkaHandler using reflection

--- src/main/java/com/telemetry/rollerskates/KafkaHandler.java
+++ src/main/java/com/telemetry/rollerskates/KafkaHandler.java
@@ -2,10 +2,6 @@ package com.telemetry.rollerskates;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.telemetry.rollerskates.entity.Humidity;
-import com.telemetry.rollerskates.entity.Pressure;
-import com.telemetry.rollerskates.entity.Speed;
-import com.telemetry.rollerskates.entity.Temperature;
 import org.springframework.integration.endpoint.MessageProducerSupport;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
@@ -13,48 +9,37 @@ import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
 
 @Component
 public class KafkaHandler extends MessageProducerSupport implements MessageHandler {
 
+    private static final String ENTITY_PACKAGE = "com.telemetry.rollerskates.entity.";
 
     @Override
     public void handleMessage(Message<?> message) throws MessagingException {
         String data = message.getPayload().toString();
-        if (data.contains("temperature")) {
-            Temperature temperature = null;
-            try {
-                temperature = new ObjectMapper().readValue(data, Temperature.class);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
+        ArrayList<String> types = new ArrayList<>();
+        types.add("Temperature");
+        types.add("Humidity");
+        types.add("Pressure");
+        types.add("Speed");
+
+        String classType = null;
+
+        for (String type : types) {
+            if (data.contains(type.toLowerCase())) {
+                classType = type;
             }
-            sendMessage(MessageBuilder.withPayload(temperature).build());
-        } else if (data.contains("humidity")) {
-            Humidity humidity = null;
-            try {
-                humidity = new ObjectMapper().readValue(data, Humidity.class);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-            sendMessage(MessageBuilder.withPayload(humidity).build());
-        } else if (data.contains("pressure")) {
-            Pressure pressure = null;
-            try {
-                pressure = new ObjectMapper().readValue(data, Pressure.class);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-            sendMessage(MessageBuilder.withPayload(pressure).build());
-        } else if (data.contains("speed")) {
-            Speed speed = null;
-            try {
-                speed = new ObjectMapper().readValue(data, Speed.class);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-            sendMessage(MessageBuilder.withPayload(speed).build());
-        } else {
-            throw new IllegalArgumentException();
+        }
+
+        try {
+            Object object = new ObjectMapper().readValue(data, Class.forName(ENTITY_PACKAGE + classType));
+            sendMessage(MessageBuilder.withPayload(object).build());
+
+        } catch (JsonProcessingException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
